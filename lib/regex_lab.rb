fbcbea1034ce37f5a1d97134a5072d908c46eb15
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-26 19:15:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- if text.match(/\A[A-Z]\w*\./)
+ if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+   #text.match(/\A[A-Z]\w*\./)
    true
  else false
  end
